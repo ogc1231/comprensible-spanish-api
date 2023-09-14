cbd6b0fe79082df5ac789bfa80471cacd4575eb9
ogc1231 <ogc@live.ie>
2023-09-14 14:17:12 +0000

    Add ResourceList & ResourceDetail views

--- resources/views.py
+++ resources/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from api.permissions import IsOwnerOrReadOnly
+from .models import Resource
+from .serializers import ResourceSerializer
 
-# Create your views here.
+
+class ResourceList(generics.ListCreateAPIView):
+
+    serializer_class = ResourceSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Resource.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class ResourceDetail(generics.RetrieveUpdateDestroyAPIView):
+
+    serializer_class = ResourceSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Resource.objects.all()
