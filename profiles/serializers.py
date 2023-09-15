d238fe8efb0dea07bb0a36ad63633cf965ca68db
ogc1231 <ogc@live.ie>
2023-09-15 08:30:53 +0000

    Add number of resoucres by porfile filters

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -5,6 +5,7 @@ from .models import Profile
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    resources_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -14,5 +15,5 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = Profile
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name',
-            'image', 'is_owner'
+            'image', 'is_owner', 'resources_count',
         ]
