5d0670b9ad75f4e83834c227933a71a13f01228e
ogc1231 <ogc@live.ie>
2023-09-15 08:18:15 +0000

    Add favourite_id field to resources serializer

--- resources/serializers.py
+++ resources/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from resources.models import Resource
+from favourites.models import Favourite
 
 
 class ResourceSerializer(serializers.ModelSerializer):
@@ -7,6 +8,7 @@ class ResourceSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    favourite_id = serializers.SerializerMethodField()
 
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
@@ -25,6 +27,15 @@ class ResourceSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_favourite_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            favourite = Favourite.objects.filter(
+                owner=user, resource=obj
+            ).first()
+            return favourite.id if favourite else None
+        return None
+
     class Meta:
         model = Resource
         fields = [
@@ -32,5 +43,5 @@ class ResourceSerializer(serializers.ModelSerializer):
             'profile_image', 'created_at', 'updated_at',
             'title', 'image', 'resource_url',
             'country_filter', 'resource_type_filter',
-            'difficulty_level_filter',
+            'difficulty_level_filter', 'favourite_id',
         ]
