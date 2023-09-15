db86a5ed2b0c0b9e624485353b33b56bbb2bfe69
ogc1231 <ogc@live.ie>
2023-09-15 09:57:56 +0000

    Add client origin

--- api/urls.py
+++ api/urls.py
@@ -23,7 +23,10 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('dj-rest-auth/logout/', logout_route),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('dj-rest-auth/registration/', include(
+        'dj_rest_auth.registration.urls'
+        )
+    ),
     path('', include('profiles.urls')),
     path('', include('resources.urls')),
     path('', include('favourites.urls')),
